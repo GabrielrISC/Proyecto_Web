ceb82dc3b9ba6926342fca3f10a1a2a15a252946
ACC-PC3\Admin <mht@mhtjcv.onmicrosoft.com>
2021-11-17 09:33:26 -0700

    Primer paso del proyecto, creacion de la conexion y objeto estatus

---
+++ Servicios/DB.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using Datos;
+using System.Data;
+
+namespace Servicios
+{
+    public class DB
+    {
+        public SqlConnection conexion= new SqlConnection("Server=A2NWPLSK14SQL-v01.shr.prod.iad2.secureserver.net;Database=db_ProyectoWeb;User Id=sistemas_8vo;Password=3I4r7?mh;");
+        public SqlCommand comando = new SqlCommand();
+        public SqlDataReader lector = null;
+        public SqlDataAdapter adaptador = new SqlDataAdapter();
+
+         void AbrirCon()
+        {
+            this.conexion.Open();
+        }
+         void CerrarCon()
+        {
+            this.conexion.Close();
+        }
+        void verificar_cierre()
+        {
+            if (conexion.State == ConnectionState.Open)
+            {
+                conexion.Close();
+            }
+        }
+
+        public Estatus ExecutarQuery(string query)
+        {           
+            try
+            {
+                AbrirCon();// Abro la conexion
+                comando = new SqlCommand(query, conexion);// preparo el objeto command con el query
+                comando.ExecuteNonQuery();// executo el query
+                comando.Dispose();// libero el objeto
+                CerrarCon();// cierro la conexion
+                return new Estatus {Mensaje="Query ejecutado",Codigo=100,Estado="Exito"};// regreso un estatus de la ejecucion
+            }
+            catch (Exception e)
+            {
+                verificar_cierre();// verifico si la conexion se quedo abierta si es asi la cierro
+                string _mensaje = e.Message;
+                return new Estatus { Mensaje = _mensaje, Codigo = 500, Estado="Error" };// regreso un estatus de la ejecucion
+            }
+        }
+        public DataTable QuerySelect(string query)
+        {
+            DataTable _resultado = new DataTable();
+            try
+            {
+                AbrirCon();// Abro la conexion
+                comando = new SqlCommand(query, conexion);// preparo el objeto command para el query
+                adaptador = new SqlDataAdapter(comando);// le asigno el adaptador para obtener el resultado del query
+                adaptador.Fill(_resultado);// dibujo en el datatable el resultado del query
+                comando.Dispose();// libero el objeto comando
+                conexion.Close();// cierro la conexion
+                return _resultado;// regreso el datatable
+            }
+            catch (Exception e)
+            {
+                verificar_cierre();// verifico si la conexion se quedo abierta si es asi la cierro
+                List<string> errores = new List<string>() {e.Message,"500","Error" };//creo una listita temporal con informacion general del error
+                DataRow row = _resultado.NewRow();// creo una fila para agregarla al datatable
+                row.ItemArray = errores.ToArray();// convierto la lista a arreglo para insertarla en el datatable
+                return _resultado;// regreso el datatable
+            }
+        }
+    }
+}
