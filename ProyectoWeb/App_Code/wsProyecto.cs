51343e107aa2e3063580ecad4d5c3f27860a43f5
ACC-PC3\Admin <mht@mhtjcv.onmicrosoft.com>
2021-11-18 12:30:16 -0700

    Adaptacion web services y front end del proyecto

---
+++ ProyectoWeb/App_Code/wsProyecto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Script.Services;
+using System.Web.Services;
+using Newtonsoft.Json;
+
+/// <summary>
+/// Summary description for wsProyecto
+/// </summary>
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+[ScriptService]
+public class wsProyecto : System.Web.Services.WebService
+{
+    public wsProyecto()
+    {
+        //Uncomment the following line if using designed components 
+        //InitializeComponent(); 
+    }
+
+    [WebMethod]
+    public string Iniciar_Sesion(string usuario,string contrasena)
+    {       
+        Dictionary<string, object> dichola = new Dictionary<string, object>();
+        dichola.Add("User", usuario);
+        dichola.Add("Pass", contrasena);
+
+        return JsonConvert.SerializeObject(dichola);
+    }
+
+}
---
+++ ProyectoWeb/js/login.js
@@ -0,0 +1,21 @@
+﻿$('.login').submit(function (eve) {
+
+    eve.preventDefault();
+    let _datos = { "usuario": $('#txtUsr').val(), "contrasena": $('#txtContr').val() };
+    $.ajax({
+        type: 'POST',
+        url: 'wsProyecto.asmx/Iniciar_Sesion',
+        data: JSON.stringify(_datos),
+        contentType: 'application/json; charset=utf-8',
+        dataType: 'json',
+        async: true,
+        success: function (respuesta) {
+            let _valores = JSON.parse(respuesta.d);
+            console.log(_valores);
+        },
+        error: function (XMLHttpRequest, textstatus, errorthrown) {
+            console.log(errorthrown);
+        }
+
+    });
+});
